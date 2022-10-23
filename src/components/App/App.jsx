46bd70cb3a9b564848ceb99712d5589e9171d12e
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2022-10-23 12:31:32 +0300

    refactoring #1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FeedbackForm } from './App.styled';
 
-export const Feedback = () => {
+export function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -43,4 +43,4 @@ export const Feedback = () => {
       </Section>
     </FeedbackForm>
   );
-};
+}
